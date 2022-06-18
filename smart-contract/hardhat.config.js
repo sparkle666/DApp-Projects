f662be591217535456ad32f2f7f7e8fd6e595893
Sixtus Anyanwu <sikky606@gmail.com>
2022-06-18 22:06:08 +0100

    added dotenv

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,13 +1,13 @@
 // https://eth-ropsten.alchemyapi.io/v2/kUvmyTaG19kBmcK25eq6vujzwfTwr92w
-
+require('dotenv').config()
 require('@nomiclabs/hardhat-waffle')
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/kUvmyTaG19kBmcK25eq6vujzwfTwr92w',
-      accounts: ['956cd6fc6331f468b6e10e06631af584ac7ecbd5c0bfb2b1202d73bad8069f4a']
+      url: process.env.ALCHEMY_NODE,
+      accounts: [process.env.PRIVATE_METAMASK_KEY]
     }
   }
 }
